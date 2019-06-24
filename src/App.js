d7eb0363b53515ee556bab9d5ba8b636f37f5641
cmariofonseca <cmariofonseca@gmail.com>
2019-06-24 12:16:57 -0500

    new commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Label, List, Checkbox } from "semantic-ui-react";
+import products from './App.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const categories = [];
+let arraySelected = [];
+
+class ProductsTreeSelect extends React.Component {
+
+  state = {
+    selected: false
+  };
+
+  handleSelected = item => {
+    arraySelected.push(item);
+    console.log(':)');
+  }
+
+  handleTreeSelect = products => {
+    let levelOne = this.handleLevel(products);
+    return levelOne;
+  }
+
+  handleLevel = products => {
+    let temp = [];
+    let newElements = products['children'];
+    for (let i = 0; i < newElements.length; i++) {
+      temp.push(
+        <List.Item>
+          <List.Icon><Checkbox/></List.Icon>
+          <List.Content>
+            <List.Header>{newElements[i].label}</List.Header>
+            {
+              newElements[i].hasOwnProperty('children')
+              ? <List.List>{this.handleTreeSelect(newElements[i])}</List.List>
+              : ''
+            } 
+          </List.Content>
+        </List.Item>
+      );
+    }
+    return temp;
+  }
+
+  handleSublevel = products => {
+    let temp = [];
+    let newElements = products['children'];
+    for (let i = 0; i < newElements.length; i++) {
+      temp.push(
+        <List.Item>
+          <List.Icon><Checkbox /></List.Icon>
+          <List.Content>
+            <List.Header>{newElements[i].label}</List.Header>
+          </List.Content>
+        </List.Item>
+      );
+    }
+    return temp;
+  }
+
+  render() {
+
+    for (let i = 0; i < products.length; i++) {
+      categories.push(
+      );
+    }
+
+    console.log(categories);
+
+    return (
+      <List>
+        {
+          products.map(category => (
+            <List.Item>
+              <List.Icon><Checkbox /></List.Icon>
+              <List.Content>
+                <List.Header>{ category.label }</List.Header>
+                {
+                  category.hasOwnProperty('children')
+                  ?
+                  <List.List>
+                    {
+                      category['children'].map(levelOne => (
+                        <List.Item>
+                          <List.Icon><Checkbox /></List.Icon>
+                          <List.Content>
+                            <List.Header>{levelOne.label}</List.Header>
+                          </List.Content>
+                        </List.Item>
+                      ))
+                    }
+                  </List.List>
+                  :
+                  ''
+                }              
+              </List.Content>
+            </List.Item>
+          ))
+        }
+      </List>
+    );
+
+  }
+
 }
 
-export default App;
+export default ProductsTreeSelect;
